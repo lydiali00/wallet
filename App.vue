292e33aaa1dd5b3f7196a72c1cdca12b528d2196
lydiali00 <1261142171@qq.com>
2023-01-15 13:43:53 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,16 @@
+<template>
+  <w3m-core-button></w3m-core-button>
+  <router-view/>
+  <web3Modal />
+</template>
+<script>
+import web3Modal from './HtmlWallet'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  components: {
+    web3Modal
+  }
+})
+
+</script>
